db0919d677da9bae53acb1772d357269f0a3fbeb
Anwesh43 <anweshthecool0@gmail.com>
2019-10-31 19:49:31 +0530

    adding ScaleUtil class

--- index.ts
+++ index.ts
@@ -37,3 +37,18 @@ class Stage {
         stage.handleTap()
     }
 }
+
+class ScaleUtil {
+
+    static maxScale(scale : number, i : number, n : number) : number {
+        return Math.max(0, scale - i / n)
+    }
+
+    static divideScale(scale : number, i : number, n : number) : number {
+        return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n
+    }
+
+    static sinify(scale : number) : number {
+        return Math.sin(Math.PI * scale)
+    }
+}
